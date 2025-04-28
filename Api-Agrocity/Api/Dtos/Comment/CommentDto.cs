2ab33e71e6e7cea16d79fc0e35ff1eca3f7f433a
Pabloea97 <eapabloea@gmail.com>
2025-04-27 21:23:27 -0600

    Crud Comments
    Creation of the CRUD to manage comment logic

---
+++ Api-Agrocity/Api/Dtos/Comment/CommentDto.cs
@@ -0,0 +1,20 @@
+namespace Api.Dtos.Comment
+{
+    public class CommentDto
+    {
+        public int CommentId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string? UserName { get; set; } 
+
+        public int? PublicationId { get; set; }
+
+         public string? PublicationTitle { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+    }
+}
