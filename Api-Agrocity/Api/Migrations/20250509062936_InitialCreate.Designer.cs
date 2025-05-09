49b9962f95de33446f4a9da7c009bae99aacc800
JordiFRB <jordirivasbf@gmail.com>
2025-05-09 01:28:41 -0600

    Ahora es posible recuperar contraseña mediante un envio de token temporal al correo del usuario para resetear la contraseña olvidada

--- Api-Agrocity/Api/Migrations/20250509062936_InitialCreate.Designer.cs
+++ Api-Agrocity/Api/Migrations/20250509062936_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Migrations
 {
     [DbContext(typeof(UrbanGardeningContext))]
-    [Migration("20250329091939_InitialCreate")]
+    [Migration("20250509062936_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -34,8 +34,8 @@ namespace Api.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("CalendarId"));
 
-                    b.Property<DateOnly?>("CalendarDate")
-                        .HasColumnType("date")
+                    b.Property<DateTime?>("CalendarDate")
+                        .HasColumnType("datetime2")
                         .HasColumnName("calendarDate");
 
                     b.Property<string>("Description")
